3238cee4ebbd2fdde9b84406b2435adaca5c3ec9
Mario D’Arco <mario.darco@onthebeach.co.uk>
2017-06-30 13:36:48 +0100

    Prepare failing tests for argument defaults

--- spec/selfies/self_init_spec.rb
+++ spec/selfies/self_init_spec.rb
@@ -46,19 +46,65 @@ RSpec.describe Selfies::SelfInit do
           expect(instance.colour).to eql :red
         end
       end
+
+      context 'when given arguments do not match variable names' do
+        let(:expected_message) do
+          'wrong number of arguments (given 2, expected 3)'
+        end
+        let(:variable_names) { %i[type colour price] }
+
+        before { subject }
+
+        it 'raises an ArgumentError' do
+          expect { instance }.to raise_error(ArgumentError, expected_message)
+        end
+      end
     end
 
-    context 'when given arumnets do not match variable names' do
-      let(:expected_message) do
-        'wrong number of arguments (given 2, expected 3)'
-      end
-      let(:instance) { Car.new(:large, :blue) }
-      let(:variable_names) { %i[type colour price] }
+    context 'when last variable name has a default' do
+      let(:instance) { Car.new(:large, :red) }
+      let(:variable_names) { [:type, { colour: :blue }] }
 
       before { subject }
 
-      it 'raises an ArgumentError' do
-        expect { instance }.to raise_error(ArgumentError, expected_message)
+      it 'generates an initializer method for those attributes' do
+        expect(instance).to be_a Car
+      end
+
+      it 'generates readers for the given attributes' do
+        expect(instance.type).to eql :large
+        expect(instance.colour).to eql :red
+      end
+
+      context 'and the object is then initialised without that parameter' do
+        let(:instance) { Car.new(:large) }
+
+        it 'assign default to the last attribute' do
+          expect(instance.colour).to eql :blue
+        end
+      end
+
+      context 'when given arguments do not match variable names' do
+        context 'by one more' do
+          let(:expected_message) do
+            'wrong number of arguments (given 3, expected 2)'
+          end
+          let(:instance) { Car.new(:large, :red, :expensive) }
+
+          before { subject }
+
+          it 'raises an ArgumentError' do
+            expect { instance }.to raise_error(ArgumentError, expected_message)
+          end
+        end
+
+        context 'by one less' do
+          let(:instance) { Car.new(:large) }
+
+          it 'does not raise error' do
+            expect { instance }.to_not raise_error
+          end
+        end
       end
     end
   end
