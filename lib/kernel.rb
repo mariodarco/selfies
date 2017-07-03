7e1aaf6cd356566b572ed89686bceb0001d8dbd8
Mario D’Arco <mario.darco@onthebeach.co.uk>
2017-07-03 22:15:42 +0100

    Remove deprecated method

--- lib/kernel.rb
+++ lib/kernel.rb
@@ -10,11 +10,6 @@ module Kernel
     Selfies.generate_initializer(self, false, *variable_names)
   end
 
-  def self_init(*variable_names)
-    attr_reader_init(*variable_names)
-  end
-  deprecate :self_init, :attr_reader_init, 2017, 7
-
   def selfie(*method_names)
     Selfies.generate_class_methods(self, *method_names)
   end
