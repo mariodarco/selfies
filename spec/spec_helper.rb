43e7b51f98c75ff9f132f6a37ed317be10fa2a9f
Mario D’Arco <mario.darco@onthebeach.co.uk>
2017-08-04 17:46:54 +0100

    Remove coverage statistics

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,5 @@
 require 'bundler/setup'
 require 'selfies'
-require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-  add_group 'Library', 'lib'
-end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
